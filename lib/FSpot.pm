c2a3fbedb13f91e116267e8d5bbb1222752a2620
Ivan Wills <ivan.wills@gmail.com>
2009-06-17 12:25:19 +1000

    Uncatalised the project

--- lib/FSpot.pm
+++ lib/FSpot.pm
@@ -1,64 +1,201 @@
 package FSpot;
 
-use strict;
-use warnings;
+# Created on: 2009-06-17 09:30:10
+# Create by:  Ivan Wills
+# $Id$
+# $Revision$, $HeadURL$, $Date$
+# $Revision$, $Source$, $Date$
 
-use Catalyst::Runtime 5.80;
+use Moose;
+use version;
+use Carp qw/cluck/;
+use Data::Dumper qw/Dumper/;
+use English qw/ -no_match_vars /;
+use base qw/Exporter/;
+use Config::General;
+use FSpot::Schema;
+use FindBin qw/$Bin/;
+use Path::Class;
 
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
+our $VERSION     = version->new('0.0.1');
+our @EXPORT_OK   = qw//;
+our %EXPORT_TAGS = ();
 
-use parent qw/Catalyst/;
-use Catalyst qw/-Debug
-                ConfigLoader
-                Static::Simple/;
-our $VERSION = '0.01';
+has config => (
+	is      => 'rw',
+	#isa     => 'Config::General',
+);
 
-# Configure the application.
-#
-# Note that settings in fspot.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with an external configuration file acting as an override for
-# local deployment.
+has config_file => (
+	is      => 'rw',
+	#isa     => 'Path::Class::File',
+	builder => '_build_config_file',
+	trigger => \&_build_config,
+);
 
-__PACKAGE__->config( name => 'FSpot' );
+has _schema => (
+	is  => 'rw',
+	isa => 'FSpot::Schema',
+);
 
-# Start the application
-__PACKAGE__->setup();
+sub _build_config {
+	my ($self, $file) = @_;
 
+	if (!defined $file) {
+		cluck "no config file yet\n";
+	}
+	elsif (-e $file) {
+		my $cfg    = Config::General->new($file);
+		$self->config({ $cfg->getall });
+
+		$self->config->{connect_info} ||= "dbi:SQLite:$ENV{HOME}/.gnome2/f-spot/photos.db";
+	}
+	else {
+		$self->config({ connect_info => "dbi:SQLite:$ENV{HOME}/.gnome2/f-spot/photos.db" });
+	}
+
+	return;
+}
+
+sub _build_config_file {
+	my $self = shift;
+
+	return if defined $self->config_file;
+
+	$self->config_file(
+		-f "$ENV{HOME}/.fspot-tools.conf" ? file "$ENV{HOME}/.fspot-tools.conf"
+		: "$Bin/../fspot-tools.conf"      ? file "$Bin/../fspot-tools.conf"
+		:                                   file './fspot-tools.conf'
+	);
+
+	$self->_build_config($self->config_file);
+
+	return;
+}
+
+sub schema {
+	my $self = shift;
+
+	return $self->_schema if $self->_schema;
+
+	my $schema   = FSpot::Schema->connect( $self->config->{connect_info} );
+
+	return $self->_schema($schema);
+}
+
+1;
+
+__END__
 
 =head1 NAME
 
-FSpot - Catalyst based application
+FSpot - <One-line description of module's purpose>
+
+=head1 VERSION
+
+This documentation refers to FSpot version 0.1.
+
 
 =head1 SYNOPSIS
 
-    script/fspot_server.pl
+   use FSpot;
+
+   # Brief but working code example(s) here showing the most common usage(s)
+   # This section will be as far as many users bother reading, so make it as
+   # educational and exemplary as possible.
+
 
 =head1 DESCRIPTION
 
-[enter your description here]
+A full description of the module and its features.
 
-=head1 SEE ALSO
+May include numerous subsections (i.e., =head2, =head3, etc.).
 
-L<FSpot::Controller::Root>, L<Catalyst>
 
-=head1 AUTHOR
+=head1 SUBROUTINES/METHODS
 
-Ivan Wills,,,
+A separate section listing the public components of the module's interface.
 
-=head1 LICENSE
+These normally consist of either subroutines that may be exported, or methods
+that may be called on objects belonging to the classes that the module
+provides.
 
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
+Name the section accordingly.
+
+In an object-oriented module, this section should begin with a sentence (of the
+form "An object of this class represents ...") to give the reader a high-level
+context to help them understand the methods that are subsequently described.
+
+
+=head3 C<new ( $search, )>
+
+Param: C<$search> - type (detail) - description
+
+Return: FSpot -
+
+Description:
 
 =cut
 
-1;
+=head1 DIAGNOSTICS
+
+A list of every error and warning message that the module can generate (even
+the ones that will "never happen"), with a full explanation of each problem,
+one or more likely causes, and any suggested remedies.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+A full explanation of any configuration system(s) used by the module, including
+the names and locations of any configuration files, and the meaning of any
+environment variables or properties that can be set. These descriptions must
+also include details of any configuration language used.
+
+=head1 DEPENDENCIES
+
+A list of all of the other modules that this module relies upon, including any
+restrictions on versions, and an indication of whether these required modules
+are part of the standard Perl distribution, part of the module's distribution,
+or must be installed separately.
+
+=head1 INCOMPATIBILITIES
+
+A list of any modules that this module cannot be used in conjunction with.
+This may be due to name conflicts in the interface, or competition for system
+or program resources, or due to internal limitations of Perl (for example, many
+modules that use source code filters are mutually incompatible).
+
+=head1 BUGS AND LIMITATIONS
+
+A list of known problems with the module, together with some indication of
+whether they are likely to be fixed in an upcoming release.
+
+Also, a list of restrictions on the features the module does provide: data types
+that cannot be handled, performance issues and the circumstances in which they
+may arise, practical limitations on the size of data sets, special cases that
+are not (yet) handled, etc.
+
+The initial template usually just has:
+
+There are no known bugs in this module.
+
+Please report problems to Ivan Wills (ivan.wills@gmail.com).
+
+Patches are welcome.
+
+=head1 AUTHOR
+
+Ivan Wills - (ivan.wills@gmail.com)
+<Author name(s)>  (<contact address>)
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2009 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+
+=cut
